00e76df90e97c296e5ee46a97620b83f3602c04a
Luka Janelidze <lukajanelidze5@gmail.com>
2024-03-03 20:57:47 +0400

    fix: fix pictures

--- src/assets/components/FirstSection/FirstSection.jsx
+++ src/assets/components/FirstSection/FirstSection.jsx
@@ -14,19 +14,19 @@ export default function FirstSection() {
                     <h3 className='first-step'>Step 1</h3>
                     <p>To receive the airdrop, post on X why you are bullish on $AILU and submit the link to your post in the "Insert Link" section.</p>
                     <div className="icon-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 408c0 13.3-10.7 24-24 24s-24-10.7-24-24V305.9l-31 31c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l72-72c9.4-9.4 24.6-9.4 33.9 0l72 72c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-31-31V408z"/></svg>
                     </div>
                 </div>
 
             <hr />
 
-            <div className="firstsection-wrap-steps-second">
-                <h3 className='second-step'>Step 2</h3>
-                <p>Enter your wallet address to become eligible for $AILU airdrop</p>
-                <div className="icon-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 408c0 13.3-10.7 24-24 24s-24-10.7-24-24V305.9l-31 31c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l72-72c9.4-9.4 24.6-9.4 33.9 0l72 72c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-31-31V408z"/></svg>
+                <div className="firstsection-wrap-steps-second">
+                    <h3 className='second-step'>Step 2</h3>
+                    <p>Enter your wallet address to become eligible for $AILU airdrop</p>
+                    <div className="icon-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V192c0-35.3-28.7-64-64-64H80c-8.8 0-16-7.2-16-16s7.2-16 16-16H448c17.7 0 32-14.3 32-32s-14.3-32-32-32H64zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
+                    </div>
                 </div>
-            </div>
 
             </div>
 
